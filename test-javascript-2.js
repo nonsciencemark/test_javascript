a496d28c02c8f99ae212115f84a17ef26530cce1
nonsciencemark <markjamestheholmes@gmail.com>
2022-06-19 14:37:24 +0200

    this seems to work and yet it does not

--- test-javascript-2.js
+++ test-javascript-2.js
@@ -1,18 +1,20 @@
-// function to create number array
+// function to create number array stolen from internet
 function generateArrayOfNumbers(numbers) {
     return [...Array(numbers).keys()].slice(1)
 };
 
-// rolls from 1-20
+// dice rolls from 1-20
 var rolls = generateArrayOfNumbers(21);
 console.log(rolls); // view
 
 // what is the modifier for a given roll?
 function getMod(roll) {
-    let mod = Math.round((roll - 11) / 2);
-    return mod;
-};
+    let mod = Math.round((roll - 11) / 2); // this is how the mods work
+    console.log(mod);
+}; // why is this console log? why doesn't return work?
 
 // apply that function to the rolls array
-console.log(getMod.apply(null, rolls));
-// why is this wrong?
+var mods = rolls.forEach(getMod);
+
+console.log(rolls);
+console.log(mods);
